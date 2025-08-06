ca5afa02469b9558278ed9637c0bdcca858cc7ec
si3aa <mahmoud22said22@gmail.com>
2025-08-06 18:25:22 +0300

    use queryPram as input and show 'from=appSafeLink' value in url when i click link

--- src/app/save-link.directive.ts
+++ src/app/save-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 import Swal from 'sweetalert2';
 
 @Directive({
@@ -9,38 +9,40 @@ import Swal from 'sweetalert2';
   },
 })
 export class SafeLinkDirective {
-  constructor() {
-    console.log('SafeLinkDirective is active..!');
-  }
+  queryParam = input('myapp', {alias: 'appSafeLink'});
+  // constructor() {
+  //   console.log('SafeLinkDirective is active..!');
+  // }
   //   onConfirmLeavePage(event: MouseEvent) {
   //     const wantsToLeave = window.confirm('Are You Want To Leave This Page..?');
   //     if(wantsToLeave){
+  //  const address =(event.target as HTMLAnchorElement).href;
+  // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam;
   //         return;
   //     }
   //     event.preventDefault();
   //   }
   async onConfirmLeavePage(event: MouseEvent) {
-  // Prevent the default action immediately
-  event.preventDefault();
-  
-  const result = await Swal.fire({
-    title: 'Leave Page?',
-    text: 'Are you sure you want to leave this page? Any unsaved changes will be lost.',
-    icon: 'question',
-    showCancelButton: true,
-    confirmButtonColor: '#dc3545',
-    cancelButtonColor: '#6c757d',
-    confirmButtonText: '<i class="fas fa-sign-out-alt"></i> Leave',
-    cancelButtonText: '<i class="fas fa-times"></i> Cancel',
-    reverseButtons: true,
-    focusCancel: true
-  });
+    // Prevent the default action immediately
+    event.preventDefault();
 
-  if (result.isConfirmed) {
-    const target = event.target as HTMLAnchorElement;
-    if (target && target.href) {
-      window.location.href = target.href;
+    const result = await Swal.fire({
+      title: 'Leave Page?',
+      text: 'Are you sure you want to leave this page? Any unsaved changes will be lost.',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#dc3545',
+      cancelButtonColor: '#6c757d',
+      confirmButtonText: '<i class="fas fa-sign-out-alt"></i> Leave',
+      cancelButtonText: '<i class="fas fa-times"></i> Cancel',
+      reverseButtons: true,
+      focusCancel: true,
+    });
+
+    if (result.isConfirmed) {
+      const address = (event.target as HTMLAnchorElement).href;
+      // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam;
+      window.location.href = address + '?from=' + this.queryParam;
     }
   }
-}
 }
