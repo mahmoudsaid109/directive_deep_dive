843177bb9aaef5bbbdff1fa1a5792da0022c735e
si3aa <mahmoud22said22@gmail.com>
2025-08-09 15:42:33 +0300

    use ng-template to show content after check permission

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -8,12 +8,14 @@ import { AuthService } from './auth.service';
 export class AuthDirective {
   userType = input.required<Permission>({alias: 'appAuth'});
   private authService = inject(AuthService);
+  private templateRef = inject(TemplateRef);
+  private viewContainer = inject(ViewContainerRef);
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
-        console.log('SHOW ELEMENT');
+        this.viewContainer.createEmbeddedView(this.templateRef)
       } else {
-        console.log('HIDE ELEMENT');
+        this.viewContainer.clear();
       }
     });
   }
